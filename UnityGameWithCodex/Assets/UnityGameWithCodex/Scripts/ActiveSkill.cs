18662d411c9ad8795d5e6fe8894dfdf4e23f8c25
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2026-03-20 16:16:10 +0900

    Integrate active skills with feedback

--- UnityGameWithCodex/Assets/UnityGameWithCodex/Scripts/ActiveSkill.cs
+++ UnityGameWithCodex/Assets/UnityGameWithCodex/Scripts/ActiveSkill.cs
@@ -1,14 +1,23 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using HKFeedback;
 using UnityEngine;
 
 namespace UnityGameWithCodex
 {
     public class ActiveSkill : ScriptableObject
     {
+        [SerializeReference, SubclassSelector]
+        private IFeedback<BattleContext> feedback;
+
         public virtual UniTask InvokeAsync(BattleContext battleContext)
         {
-            Debug.Log($"{name} invoked by {battleContext.ActingCharacter.Name}");
-            return UniTask.CompletedTask;
+            if (feedback == null)
+            {
+                return UniTask.CompletedTask;
+            }
+
+            return feedback.PlayAsync(battleContext, CancellationToken.None);
         }
     }
 }
